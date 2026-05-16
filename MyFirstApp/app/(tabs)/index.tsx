8dd769caf40154e5473587b7c887ee378a65111b
enzaurrr <jesalon@mcm.edu.ph>
2026-05-17 04:02:30 +0800

    Updated profile card to be interactive using useState and TextInput

--- MyFirstApp/app/(tabs)/index.tsx
+++ MyFirstApp/app/(tabs)/index.tsx
@@ -1,30 +1,39 @@
+import { useState } from 'react'; 
 import { Image } from 'expo-image';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, TextInput } from 'react-native'; 
 
 export default function App() {
 
   
   const myName = "Enzo"; 
-  
   const greet = (name) => {
     return `Hi ${name}! Hope you're having a great day.`; 
   };
-
-  
   const classmates = ["Vera", "Bu", "Raf", "Mish", "Sir"]; 
-
- 
   console.log(classmates.map(name => greet(name)));
   
+  
+  const [name, setName] = useState('');
 
   return (
     <View style={s.screen}>
       <Image 
-        source={{ uri: 'https://scontent.fdvo1-2.fna.fbcdn.net/v/t39.30808-6/487359228_3035059053327499_7494414871431225746_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=13d280&_nc_eui2=AeF_B3aC0wfvJhBinaYKKR9pSmVyqNaTFfhKZXKo1pMV-NB_8w__hq-c1yQExki_pouICAPEzhsR42Jz5l_Md0Dq&_nc_ohc=2-3bbTqBAUkQ7kNvwH1t_up&_nc_oc=AdreCV3tXv5KjdYAwmOavwsjhX3zygLD471_2tYolvG5pQOBIoMA643IVLiNmg3Bsv6pRd-rB6IYExT3PBco_TJG&_nc_zt=23&_nc_ht=scontent.fdvo1-2.fna&_nc_gid=XfeJiR4RAfsh1JdeKaW0rA&_nc_ss=7b2a8&oh=00_Af1ULGpiM6I84SJm9Pg4VSwiHojItZRznYH66Oj7O4ytNA&oe=69F97F1A' }} 
+        source={{ uri: 'https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/male/45.png' }} 
         style={s.photo} 
       />
       
-      <Text style={s.name}>John Enzo Salon</Text>
+      
+      <TextInput
+        placeholder="Type your name..."
+        onChangeText={setName}
+        value={name}
+        style={s.input}
+      />
+      
+      
+      <Text style={s.name}>
+        {name ? `Hello, ${name}!` : "John Enzo Salon"}
+      </Text>
       
       <Text style={s.bio}>Bachelor of Multimedia Arts</Text>
       <Text style={s.bio}>He / Him - Specializes in Film</Text>
@@ -32,7 +41,8 @@ export default function App() {
   );
 }
 
-// I change the stylesheet to make the profile screen look better. 05/01/2026
+
+// I added a TextInput component and used useState to make the name interactive. 05/15/2026
 const s = StyleSheet.create({
   screen: { 
     flex: 1, 
@@ -43,7 +53,18 @@ const s = StyleSheet.create({
   photo: { 
     width: 120, 
     height: 120, 
-    borderRadius: 60 
+    borderRadius: 60,
+    marginBottom: 10
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    width: 200,
+    borderRadius: 5,
+    textAlign: 'center'
   },
   name: { 
     fontSize: 22, 
